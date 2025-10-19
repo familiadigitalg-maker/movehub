f4abe60020ea790837f0485eff7337dfa15aebb0
familiadigitalg-maker <familiadigitalg@gmail.com>
2025-10-19 19:28:33 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/06f0b7a504bb413ea1fc496ca8240b9f"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/76b94b4e61b247409164091cdb358d86"))()
