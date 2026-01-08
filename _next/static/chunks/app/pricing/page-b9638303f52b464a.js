8a5b1f3d0af4ae448a2193be570cb63d1ef8d9c6
familiadigitalg-maker <familiadigitalg@gmail.com>
2026-01-08 02:15:02 -0300

    Update page-b9638303f52b464a.js

--- _next/static/chunks/app/pricing/page-b9638303f52b464a.js
+++ _next/static/chunks/app/pricing/page-b9638303f52b464a.js
@@ -272,8 +272,8 @@ let g = (planType, planPrice) => {
         <div class="payment-option flex items-center justify-between p-4 border border-border rounded cursor-pointer text-base transition-all duration-400" data-method="PayPal">
           <span class="glow"></span>
           PayPal <span>${
-            planType === "weekly" ? "$2.5" :
-            planType === "monthly" ? "$6" :
+            planType === "weekly" ? "$2" :
+            planType === "monthly" ? "$4" :
             planType === "lifetime" ? "$10" : ""
           }</span>
         </div>
@@ -290,9 +290,9 @@ let g = (planType, planPrice) => {
         <div class="payment-option flex items-center justify-between p-4 border border-border rounded cursor-pointer text-base transition-all duration-400" data-method="Pix">
           <span class="glow"></span>
           Pix <span>${
-            planType === "weekly" ? "R$ 7,00" :
-            planType === "monthly" ? "R$ 9,00" :
-            planType === "lifetime" ? "R$ 17,00" : ""
+            planType === "weekly" ? "R$ 10,00" :
+            planType === "monthly" ? "R$ 20,00" :
+            planType === "lifetime" ? "R$ 35,00" : ""
           }</span>
         </div>
       </div>
@@ -336,8 +336,8 @@ let g = (planType, planPrice) => {
     const hasValidCoupon = couponCode !== "" && validCoupons.includes(couponCode);
 
     if (selectedMethod === "PayPal") {
-      if (planType === "weekly") url = "https://www.paypal.com/donate/?business=6BCAN9W5KGE3A&amount=2.5&no_recurring=1&item_name=Move+Hub+Week&currency_code=USD";
-      else if (planType === "monthly") url = "https://www.paypal.com/donate/?business=6BCAN9W5KGE3A&amount=6&no_recurring=1&item_name=Move+Hub+Month&currency_code=USD";
+      if (planType === "weekly") url = "https://www.paypal.com/donate/?business=6BCAN9W5KGE3A&amount=2&no_recurring=1&item_name=Move+Hub+Week&currency_code=USD";
+      else if (planType === "monthly") url = "https://www.paypal.com/donate/?business=6BCAN9W5KGE3A&amount=4&no_recurring=1&item_name=Move+Hub+Month&currency_code=USD";
       else url = "https://www.paypal.com/donate/?business=6BCAN9W5KGE3A&amount=10&no_recurring=1&item_name=Move+Hub+Lifetime&currency_code=USD";
     } else if (selectedMethod === "Robux") {
       if (planType === "weekly") url = "https://www.roblox.com/catalog/86763827406678/Weekly";
@@ -471,6 +471,7 @@ function Z() {
     }), _N_E = e.O()
   }
 ]);
+
 
 
 
